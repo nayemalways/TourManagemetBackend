820d576fec055dd78ce8aa446d1d6ec28085ca93
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-07-27 01:14:35 +0600

    No route matching handler added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { router } from './app/routes';
 import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
+import httpStatus from 'http-status-codes';
 
 
 const app = express();
@@ -19,5 +20,13 @@ app.use('/api/v1', router);
 
 // GLOBAL ERROR HANDLER
 app.use(globalErrorHandler);
+
+
+// NO ROUTE MATCH
+app.use((req: Request, res: Response) => {
+    res.status(httpStatus.NOT_FOUND).json({
+        message: "No Route Found"
+    })
+})
 
 export default app;
