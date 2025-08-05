73c7cf329532716d8bd0b51d4ef21803264336b3
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-08-05 22:15:54 +0600

    added Default Super Admin created functionality

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,11 +3,12 @@ import { UserZodSchema } from "./user.validate";
 import { Router } from "express";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { checkAuth } from "../../middlewares/auth.middleware";
+import { Role } from "./user.interface";
 
 
 const router = Router();
 
 router.post('/register', validateRequest(UserZodSchema),  UserControllers.createUser);
-router.get('/get-users', checkAuth("ADMIN", "SUPERADMIN") , UserControllers.allUsers);
+router.get('/get-users', checkAuth(Role.ADMIN, Role.SUPER_ADMIN) , UserControllers.allUsers);
 
 export const UserRoute = router;
--- src/config/env.ts
+++ src/config/env.ts
@@ -9,12 +9,14 @@ interface EnvInterfaces {
     NODE_ENV: 'development' | 'production';
     JWT_EXPIRATION: string;
     BCRYPT_SALT_ROUND: string;
+    SUPER_ADMIN_GMAIL: string;
+    SUPER_ADMIN_PASSWORD: string;
     
     
 }
 
 const loadEnvVarbles = (): EnvInterfaces => {
-    const requireEnvVariables: string[] = ["PORT", "MONGO_URI", "JWT_SECRET", "NODE_ENV", "JWT_SECRET", "JWT_EXPIRATION", "BCRYPT_SALT_ROUND"];
+    const requireEnvVariables: string[] = ["PORT", "MONGO_URI", "JWT_SECRET", "NODE_ENV", "JWT_SECRET", "JWT_EXPIRATION", "BCRYPT_SALT_ROUND", "SUPER_ADMIN_PASSWORD", "SUPER_ADMIN_GMAIL"];
 
     requireEnvVariables.forEach(KEY => {
         if(!process.env[KEY]) {
@@ -28,7 +30,9 @@ const loadEnvVarbles = (): EnvInterfaces => {
         JWT_SECRET: process.env.JWT_SECRET as string,
         NODE_ENV: process.env.NODE_ENV as 'development' | 'production',
         BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
-        JWT_EXPIRATION: process.env.JWT_EXPIRATION as string
+        JWT_EXPIRATION: process.env.JWT_EXPIRATION as string,
+        SUPER_ADMIN_GMAIL: process.env.SUPER_ADMIN_GMAIL as string,
+        SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
     }
 }
 
