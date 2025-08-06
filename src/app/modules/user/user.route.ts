1c934bee0aafbe1f2f1511f65f160949bfd4bc48
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-08-07 00:37:10 +0600

    user update info validate using zod

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,5 +1,5 @@
 import { UserControllers } from "./user.controller";
-import { UserZodSchema } from "./user.validate";
+import { UserUpdateZodSchema, UserZodSchema } from "./user.validate";
 import { Router } from "express";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { checkAuth } from "../../middlewares/auth.middleware";
@@ -10,6 +10,6 @@ const router = Router();
 
 router.post('/register', validateRequest(UserZodSchema),  UserControllers.createUser);
 router.get('/get-users', checkAuth(Role.ADMIN, Role.SUPER_ADMIN) , UserControllers.allUsers);
-router.patch('/:userId', checkAuth(...Object.values(Role)) , UserControllers.updateUser);
+router.patch('/:userId', validateRequest(UserUpdateZodSchema), checkAuth(...Object.values(Role)) , UserControllers.updateUser);
 
 export const UserRoute = router;
