2d098ea8439a5b6f0bc33276cd6908cf61e830d1
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-09-30 18:52:14 +0600

    created Booking zod schema, booking model

---
+++ src/app/modules/booking/booking.interface.ts
@@ -0,0 +1,17 @@
+import { Types } from "mongoose";
+
+
+export enum  IBookingStatus {
+    PENDING = 'PENDING',
+    FAILED = 'FAILED',
+    CANCEL = 'CANCEL',
+    COMPLETE = 'COMPLETE'
+}
+
+export interface IBooking {
+    user: Types.ObjectId;
+    tour: Types.ObjectId;
+    payment? : Types.ObjectId;
+    guestCount : number;
+    status: IBookingStatus
+}
---
+++ src/app/modules/booking/booking.zodSchema.ts
@@ -0,0 +1,10 @@
+import z from "zod";
+import { IBookingStatus } from "./booking.interface";
+
+export const createBookingZodSchema = z.object({
+    tour: z.string(),
+    guestCount: z.number().int().positive()
+})
+export const createBookingUpdateZodSchema = z.object({
+    status: z.enum(Object.values(IBookingStatus))
+})
