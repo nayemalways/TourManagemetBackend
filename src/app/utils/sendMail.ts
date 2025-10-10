c6ffd14f4f38f6061f23eabb4d5d2bebfe9042f9
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-10-11 00:11:40 +0600

    updated OTP API

--- src/app/utils/sendMail.ts
+++ src/app/utils/sendMail.ts
@@ -51,7 +51,6 @@ interface SendEmailOptions {
             }))
         })
 
-        console.log(`\u2709\uFE0F Email sent to ${to}: ${info.messageId}`);
     } catch (error: any) {
         console.log("Email sending error", error.message);
         throw new AppError(400, "Email error")
