c1434cd9afa466ef42914c298eefa293ff9cc42a
nayemalways <nishanahmed13913@gmail.com>
2025-11-20 10:47:56 +0600

    added cloudinary image type avif

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -53,13 +53,12 @@ export const uploadBufferToCloudinary = async (
 
 export const deleteImageFromCLoudinary = async (url: string) => {
   try {
-    const regex = /\/v\d+\/(.*?)\.(jpg|jpeg|png|gif|webp)$/i;
+    const regex = /\/v\d+\/(.*?)\.(jpg|jpeg|png|gif|webp|avif)$/i;
     const match = url.match(regex);
 
     if (match && match[1]) {
       const public_id = match[1];
       await cloudinary.uploader.destroy(public_id);
-      console.log(`File ${public_id} is deleted from cloudinary`);
     }
   } catch (error: any) {
     throw new AppError(401, 'Cloudinary image deletion failed', error.message);
