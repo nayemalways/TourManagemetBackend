5a89af570ccee962d446eb9f225a1f757ccd9813
nayemalways <nishanahmed13913@gmail.com>
2025-12-02 16:20:22 +0600

    Increased server json file sized limit

--- src/app.ts
+++ src/app.ts
@@ -22,13 +22,13 @@ app.use(
 app.use(passport.initialize()); // Initialized passport
 app.use(passport.session()); // Create a session and handled all the thing
 app.set("trust proxy", 1);
-app.use(express.json());
+app.use(express.json({limit: '20mb'}));
 app.use(cors({
   origin: env.FRONTEND_URL,
   credentials: true
 }));
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 app.use('/api-docs', swaggorUI.serve, swaggorUI.setup(swaggerSpec));
 
 app.get('/', (req: Request, res: Response) => {
